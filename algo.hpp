d5c9cd2110b4073c86a396a9b6290cac2c150189
kwabe007 <kwabena.ap@gmail.com>
2016-11-12 20:13:54 +0100

    fixed a simple fermat with room for improvements

--- algo.hpp
+++ algo.hpp
@@ -6,10 +6,63 @@
 
 using namespace boost::multiprecision;
 
-//function for prime factorization using fermats factorization method
-std::list<int128_t> fermats(int128_t n) {
-std::list<int128_t> result = {-1};
+unsigned long long is_square(unsigned long long n){
+  unsigned long long squareRootN=(unsigned long long)std::round((std::sqrt(n)));
 
+  if(squareRootN*squareRootN == n) {
+    return squareRootN;
+  }
+  else {
+    return 0LL;
+  }
+
+}
+
+//function for prime factorization using fermats factorization method
+std::list<unsigned long long> fermats(unsigned long long n) {
+std::list<unsigned long long> queue = {n};
+std::list<unsigned long long> result = {};
+unsigned long long sqr_number;
+
+
+while (!queue.empty()) {
+
+  unsigned long long current_number = queue.front();
+  queue.pop_front();
+  bool is_prime = true;
+  
+  if (!(current_number & 1LL) && current_number != 2LL) {
+    queue.push_back(2LL);
+    queue.push_back(current_number/2);
+    continue;
+    
+  }
+  
+  for (sqr_number = 1; sqr_number < current_number; ++sqr_number ) {
+    unsigned long long pow_of_sqr_number = std::pow(sqr_number, 2);
+    unsigned long long sum = current_number + pow_of_sqr_number;
+    unsigned long long integer_square_sum = is_square(sum);
+    
+    if (integer_square_sum > 0LL) {
+      unsigned long long factor1 = integer_square_sum - sqr_number;
+      unsigned long long factor2 = integer_square_sum + sqr_number;
+      
+      if (factor1 == 1LL || factor2 == 1LL)
+	break;
+      
+      queue.push_back(factor1);
+      queue.push_back(factor2);
+      is_prime = false;
+      break;
+    }
+    
+  }
+  
+  if (is_prime) {
+    result.push_back(current_number);
+  }
+  
+ }
 
 return result;
 }
--- test.cpp
+++ test.cpp
@@ -6,10 +6,21 @@ int main() {
   int i = 0;
   long l = 0L;
   long long ll = 0LL;
-  boost::multiprecision::int128_t int128 = 0; 
+  boost::multiprecision::int128_t int128 = 0;
+  unsigned  __int128 i128 = 0;
   std::cout << sizeof(i) << std::endl;
   std::cout << sizeof(l) << std::endl;
   std::cout << sizeof(ll) << std::endl;
   std::cout << sizeof(int128) << std::endl;
+  std::cout << sizeof(i128) << std::endl;
+  //std::cout << i128 << std::endl;
+
+  std::list<unsigned long long> queue = fermats(548LL);
+
+  for (unsigned long long i : queue) {
+    std::cout << i << std::endl;
+  }
   
+  
+  return 0;
 }
